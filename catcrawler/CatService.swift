ec8cdcc8942ffd62339565afc9e44726f55a9b47
sesang06 <sesang06@naver.com>
2021-07-18 11:30:30 +0900

    First Commit

---
+++ catcrawler/CatService.swift
@@ -0,0 +1,66 @@
+//
+//  CatService.swift
+//  catcrawler
+//
+//  Created by sesang on 2021/07/14.
+//
+
+import Foundation
+
+
+struct CatResponse: Codable {
+    let id: String
+    let url: String
+    let width: Int
+    let height: Int
+}
+
+final class CatService {
+    
+    
+    enum RequestError: Error {
+        case networkError
+    }
+    
+    func getCats(
+        page: Int,
+        limit: Int,
+        completion: @escaping (Result<[CatResponse], RequestError>) -> Void
+    ) {
+        
+        var components = URLComponents(string: "https://api.thecatapi.com/v1/images/search")!
+        components.queryItems = [
+            URLQueryItem(name: "page", value: "\(page)"),
+            URLQueryItem(name: "limit", value: "\(limit)")
+        ]
+        
+        var request = URLRequest(url: components.url!)
+        request.httpMethod = "GET"
+        
+        let task = URLSession.shared.dataTask(with: request) {
+            data, response, error in
+            
+            guard error == nil else {
+                completion(.failure(.networkError))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(.networkError))
+                return
+            }
+            
+            
+            guard let response = try? JSONDecoder().decode([CatResponse].self, from: data) else {
+                completion(.failure(.networkError))
+                return
+            }
+            print(response)
+            
+            
+            completion(.success(response))
+        }
+        task.resume()
+        
+    }
+}
